60d8ab6cf3b77c4a406ba498a603e7019b7fdd29
Fabio Fonseca da Silva <fabio.silva@sensedia.com>
2021-10-19 17:18:31 -0300

    nova versão da aplicação que gera os swaggers e dicionarios

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# openapi
+![Build](https://github.com/openbanking-brasil/areadesenvolvedor/workflows/Build/badge.svg)
+![Stoplight](https://github.com/openbanking-brasil/areadesenvolvedor/workflows/Stoplight/badge.svg)
+# Repositório Open Banking Brasil
+
+Repositório para documentação e requisitos das APIs regulatórias do Open Banking Brasil. 
+
+## O que é? 
+
+O Open Banking, ou sistema financeiro aberto, é a possibilidade de clientes de produtos e serviços financeiros permitirem o compartilhamento de suas informações entre diferentes instituições autorizadas pelo Banco Central e a movimentação de suas contas bancárias a partir de diferentes plataformas e não apenas pelo aplicativo ou site do banco, de forma segura, ágil e conveniente.
+
+Mais informações em: https://www.bcb.gov.br/estabilidadefinanceira/openbanking
+
+
+## Diretórios
+
+- ### / documentation / source
+  Partes dos swaggers para montar as especificaçoes (OAS 3.0) da ultima versão de cada API do Open Banking.
+- ### / documentation / versions
+  Contém as ultimas versões e as versões anteriores das especificações (OAS 3.0) das APIs do Open Banking.
---
+++ documentation/build.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+echo "Start..."
+
+# Open Banking Fase 2
+VERSION=$(grep 'version:' source/swagger/parts/_accounts_apis_part.yml | tail -n1 | awk '{ print $2}')
+swagger-cli bundle source/swagger/parts/_accounts_apis_part.yml --outfile build/swagger/accounts/doc/${VERSION}.json --type=json && cp build/swagger/accounts/doc/${VERSION}.json versions/swagger/accounts/doc/${VERSION}.json 
+VERSION=$(grep 'version:' source/swagger/parts/_consents_apis_part.yml | tail -n1 | awk '{ print $2}')
+swagger-cli bundle source/swagger/parts/_consents_apis_part.yml --outfile build/swagger/consents/doc/${VERSION}.json --type=json && cp build/swagger/consents/doc/${VERSION}.json versions/swagger/consents/doc/${VERSION}.json
+VERSION=$(grep 'version:' source/swagger/parts/_credit_cards_apis_part.yml | tail -n1 | awk '{ print $2}')
+swagger-cli bundle source/swagger/parts/_credit_cards_apis_part.yml --outfile build/swagger/credit-cards/doc/${VERSION}.json --type=json && cp build/swagger/credit-cards/doc/${VERSION}.json versions/swagger/credit-cards/doc/${VERSION}.json
+VERSION=$(grep 'version:' source/swagger/parts/_customers_apis_part.yml | tail -n1 | awk '{ print $2}')
+swagger-cli bundle source/swagger/parts/_customers_apis_part.yml --outfile build/swagger/customers/doc/${VERSION}.json --type=json && cp build/swagger/customers/doc/${VERSION}.json versions/swagger/customers/doc/${VERSION}.json
+VERSION=$(grep 'version:' source/swagger/parts/_financings_apis_part.yml | tail -n1 | awk '{ print $2}')
+swagger-cli bundle source/swagger/parts/_financings_apis_part.yml --outfile build/swagger/financings/doc/${VERSION}.json --type=json && cp build/swagger/financings/doc/${VERSION}.json versions/swagger/financings/doc/${VERSION}.json
+VERSION=$(grep 'version:' source/swagger/parts/_invoice_financings_apis_part.yml | tail -n1 | awk '{ print $2}')
+swagger-cli bundle source/swagger/parts/_invoice_financings_apis_part.yml --outfile build/swagger/invoice-financings/doc/${VERSION}.json --type=json && cp build/swagger/invoice-financings/doc/${VERSION}.json versions/swagger/invoice-financings/doc/${VERSION}.json
+VERSION=$(grep 'version:' source/swagger/parts/_loans_apis_part.yml | tail -n1 | awk '{ print $2}')
+swagger-cli bundle source/swagger/parts/_loans_apis_part.yml --outfile build/swagger/loans/doc/${VERSION}.json --type=json && cp build/swagger/loans/doc/${VERSION}.json versions/swagger/loans/doc/${VERSION}.json
+VERSION=$(grep 'version:' source/swagger/parts/_resources_apis_part.yml | tail -n1 | awk '{ print $2}')
+swagger-cli bundle source/swagger/parts/_resources_apis_part.yml --outfile build/swagger/resources/doc/${VERSION}.json --type=json && cp build/swagger/resources/doc/${VERSION}.json versions/swagger/resources/doc/${VERSION}.json
+VERSION=$(grep 'version:' source/swagger/parts/_unarranged_accounts_overdraft_apis_part.yml | tail -n1 | awk '{ print $2}')
+swagger-cli bundle source/swagger/parts/_unarranged_accounts_overdraft_apis_part.yml --outfile build/swagger/unarranged-accounts-overdraft/doc/${VERSION}.json --type=json && cp build/swagger/unarranged-accounts-overdraft/doc/${VERSION}.json versions/swagger/unarranged-accounts-overdraft/doc/${VERSION}.json
+
+
+# Open Banking Fase 3
+VERSION=$(grep 'version:' source/swagger/parts/_payments_apis_part.yml | tail -n1 | awk '{ print $2}')
+swagger-cli bundle source/swagger/parts/_payments_apis_part.yml --outfile build/swagger/payments/doc/${VERSION}.json --type=json && cp build/swagger/payments/doc/${VERSION}.json versions/swagger/payments/doc/${VERSION}.json
+
+
+# Generate dictionary
+# linux:
+./build-dictionary.sh
+# windows:
+##sh ./build-dictionary.sh
+
+# Open Banking Fase 2
+sed -i '1s/^\(\xef\xbb\xbf\)\?/\xef\xbb\xbf/' build/swagger/accounts/doc/*.json
+sed -i '1s/^\(\xef\xbb\xbf\)\?/\xef\xbb\xbf/' build/swagger/consents/doc/*.json
+sed -i '1s/^\(\xef\xbb\xbf\)\?/\xef\xbb\xbf/' build/swagger/credit-cards/doc/*.json
+sed -i '1s/^\(\xef\xbb\xbf\)\?/\xef\xbb\xbf/' build/swagger/customers/doc/*.json
+sed -i '1s/^\(\xef\xbb\xbf\)\?/\xef\xbb\xbf/' build/swagger/financings/doc/*.json
+sed -i '1s/^\(\xef\xbb\xbf\)\?/\xef\xbb\xbf/' build/swagger/invoice-financings/doc/*.json
+sed -i '1s/^\(\xef\xbb\xbf\)\?/\xef\xbb\xbf/' build/swagger/loans/doc/*.json
+sed -i '1s/^\(\xef\xbb\xbf\)\?/\xef\xbb\xbf/' build/swagger/resources/doc/*.json
+sed -i '1s/^\(\xef\xbb\xbf\)\?/\xef\xbb\xbf/' build/swagger/unarranged-accounts-overdraft/doc/*.json
+
+# Open Banking Fase 3
+sed -i '1s/^\(\xef\xbb\xbf\)\?/\xef\xbb\xbf/' build/swagger/payments/doc/*.json
+
+# Validations
+# Open Banking Fase 2
+swagger-cli validate build/swagger/accounts/doc/*.json
+swagger-cli validate build/swagger/consents/doc/*.json
+swagger-cli validate build/swagger/credit-cards/doc/*.json
+swagger-cli validate build/swagger/customers/doc/*.json
+swagger-cli validate build/swagger/financings/doc/*.json
+swagger-cli validate build/swagger/invoice-financings/doc/*.json
+swagger-cli validate build/swagger/loans/doc/*.json
+swagger-cli validate build/swagger/resources/doc/*.json
+swagger-cli validate build/swagger/unarranged-accounts-overdraft/doc/*.json
+spectral lint build/swagger/accounts/doc/*.json
+spectral lint build/swagger/consents/doc/*.json
+spectral lint build/swagger/credit-cards/doc/*.json
+spectral lint build/swagger/customers/doc/*.json
+spectral lint build/swagger/financings/doc/*.json
+spectral lint build/swagger/invoice-financings/doc/*.json
+spectral lint build/swagger/loans/doc/*.json
+spectral lint build/swagger/resources/doc/*.json
+spectral lint build/swagger/unarranged-accounts-overdraft/doc/*.json
+# Open Banking Fase 3
+swagger-cli validate build/swagger/payments/doc/*.json
+spectral lint build/swagger/payments/doc/*.json
+
+echo "...Finish"
